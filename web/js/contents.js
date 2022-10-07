bcf103abfa6c5490a6d509621fab954044b0ee0c
Jelisa <Jelisa@users.noreply.github.com>
2022-10-07 20:13:35 +0200

    7.10.22 -  inicio carga texto via js y templates.
    Iniciar el cambio a tener el contenido en ficheros y cargarlos via js

---
+++ web/js/contents.js
@@ -0,0 +1,64 @@
+export const SPANISH_TEXT = {
+    "navigationButtons" : ["Presentación", "Educación", "Software", "Publicaciones"],
+    "introductionText" : "",
+    "educationText" : {
+        "degree" : {
+            "name": "",
+            "duration": [2008, 2013],
+            "desciption" : ""
+        },
+        "master" : {
+            "name": "",
+            "duration": [2014, 2015],
+            "desciption" : ""
+        },
+        "doctorate" : {
+            "name": "",
+            "duration": [2015, 2020],
+            "desciption" : ""
+        },
+        "courseWebApps" : {
+            "name": "",
+            "duration": [2022 ],
+            "institution" : "",
+            "desciption" : ""
+        }
+    },
+    "softwareText": {},
+    "Pulications" : [
+        {"tittle": "", "publicationDate":"", "authors": "", "publisher": "", "doi": "", "abstract": "" }
+    ],
+}
+
+export const ENGLISH_TEXT = {
+    "navigationButtons" : ["Introduction", "Education", "Software", "Publications"],
+    "introductionText" : "",
+    "educationText" : {
+        "degree" : {
+            "name": "",
+            "duration": [2008, 2013],
+            "desciption" : ""
+        },
+        "master" : {
+            "name": "",
+            "duration": [2014, 2015],
+            "desciption" : ""
+        },
+        "doctorate" : {
+            "name": "",
+            "duration": [2015, 2020],
+            "desciption" : ""
+        },
+        "courseWebApps" : {
+            "name": "",
+            "duration": [2022 ],
+            "institution" : "",
+            "desciption" : ""
+        }
+    },
+    "softwareText": {},
+    "Pulications" : [
+        {"tittle": "", "publicationDate":"", "authors": "", "publisher": "", "doi": "", "abstract": "" }
+    ],
+}
+
